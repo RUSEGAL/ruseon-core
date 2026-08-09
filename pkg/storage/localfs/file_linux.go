b44c5561bf519c2ab517b713be2262406020996d
RUSEGAL <rusegal@yandex.ru>
2026-08-09 09:18:12 +0300

    fix(storage): ignore non-critical I/O errors in sliding window cache dropping

--- pkg/storage/localfs/file_linux.go
+++ pkg/storage/localfs/file_linux.go
@@ -36,13 +36,13 @@ func (fw *FileWrapper) Write(p []byte) (n int, err error) {
 
 		// Сначала дожидаемся и очищаем предыдущий чанк (N-2)
 		if fw.lastSync > fw.lastDrop {
-			unix.SyncFileRange(fd, fw.lastDrop, fw.lastSync-fw.lastDrop, unix.SYNC_FILE_RANGE_WAIT_BEFORE|unix.SYNC_FILE_RANGE_WRITE|unix.SYNC_FILE_RANGE_WAIT_AFTER)
-			unix.Fadvise(fd, fw.lastDrop, fw.lastSync-fw.lastDrop, unix.FADV_DONTNEED)
+			_ = unix.SyncFileRange(fd, fw.lastDrop, fw.lastSync-fw.lastDrop, unix.SYNC_FILE_RANGE_WAIT_BEFORE|unix.SYNC_FILE_RANGE_WRITE|unix.SYNC_FILE_RANGE_WAIT_AFTER)
+			_ = unix.Fadvise(fd, fw.lastDrop, fw.lastSync-fw.lastDrop, unix.FADV_DONTNEED)
 			fw.lastDrop = fw.lastSync
 		}
 
 		// Запускаем асинхронную запись текущего чанка (N-1)
-		unix.SyncFileRange(fd, fw.lastSync, fw.offset-fw.lastSync, unix.SYNC_FILE_RANGE_WRITE)
+		_ = unix.SyncFileRange(fd, fw.lastSync, fw.offset-fw.lastSync, unix.SYNC_FILE_RANGE_WRITE)
 		fw.lastSync = fw.offset
 	}
 
@@ -54,7 +54,7 @@ func (fw *FileWrapper) Close() error {
 	fd := int(fw.File.Fd())
 
 	if remain > 0 {
-		unix.SyncFileRange(fd, fw.lastDrop, remain, unix.SYNC_FILE_RANGE_WAIT_BEFORE|unix.SYNC_FILE_RANGE_WRITE|unix.SYNC_FILE_RANGE_WAIT_AFTER)
+		_ = unix.SyncFileRange(fd, fw.lastDrop, remain, unix.SYNC_FILE_RANGE_WAIT_BEFORE|unix.SYNC_FILE_RANGE_WRITE|unix.SYNC_FILE_RANGE_WAIT_AFTER)
 	}
 
 	// Фиксируем метаданные (размер файла) через полноценный fsync
@@ -64,7 +64,7 @@ func (fw *FileWrapper) Close() error {
 	}
 
 	if remain > 0 {
-		unix.Fadvise(fd, fw.lastDrop, remain, unix.FADV_DONTNEED)
+		_ = unix.Fadvise(fd, fw.lastDrop, remain, unix.FADV_DONTNEED)
 	}
 
 	return fw.File.Close()
